39ea2cb772d453e20934a2c33cd02759f8ae3711
Rafiqunnabi-Zihad <117552169+Rafiqunnabi-Zihad@users.noreply.github.com>
2024-03-05 22:45:58 +0600

    Add files via upload

---
+++ Assignment 05/test4.m
@@ -0,0 +1,20 @@
+% Define the given values
+F = 150; % N
+alpha_values = linspace(0, 90, 100); % Angles in degrees
+T_MO = zeros(size(alpha_values));
+T_NO = zeros(size(alpha_values));
+
+% Calculate tension in each cable for different alpha values
+for i = 1:length(alpha_values)
+    alpha = deg2rad(alpha_values(i));
+    T_MO(i) = (200 * cos(alpha) - (200 / sqrt(3))* sin(alpha) - 150 );
+    T_NO(i) = ((200 / sqrt(3))* sin(alpha) - 200 * cos(alpha) + 150);
+end
+
+% Plot tension in each cable
+plot(alpha_values, T_MO, 'b', alpha_values, T_NO, 'r');
+xlabel('\alpha (degrees)');
+ylabel('Tension (N)');
+legend('T_{MO}', 'T_{NO}');
+title('Tension in each cable vs. \alpha');
+grid on;
