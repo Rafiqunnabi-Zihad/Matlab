3fae3dcdcd8eb875aedeb4c1026a65a85fa0efa5
Rafiqunnabi-Zihad <117552169+Rafiqunnabi-Zihad@users.noreply.github.com>
2024-03-05 22:56:48 +0600

    Update test4.m

--- Assignment 05/test4.m
+++ Assignment 05/test4.m
@@ -1,6 +1,7 @@
+clc;
 % Define the given values
 F = 150; % N
-alpha_values = linspace(0, 90, 100); % Angles in degrees
+alpha_values = linspace(0, 90, 500); % Angles in degrees
 T_MO = zeros(size(alpha_values));
 T_NO = zeros(size(alpha_values));
 
@@ -9,6 +10,9 @@ for i = 1:length(alpha_values)
     alpha = deg2rad(alpha_values(i));
     T_MO(i) = (200 * cos(alpha) - (200 / sqrt(3))* sin(alpha) - 150 );
     T_NO(i) = ((200 / sqrt(3))* sin(alpha) - 200 * cos(alpha) + 150);
+    if(abs(T_MO(i) - T_NO(i)) < 0.5)
+        equil = rad2deg(alpha)
+    end
 end
 
 % Plot tension in each cable
